4f930c94493d4099ea2e7fe499f77ad47683ea2e
aliseyedi <solafiali657@gmail.com>
2023-01-29 17:20:58 +0330

    push All

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "main-white": "#f5d0fe",
+      },
+    },
+  },
+  plugins: [],
+};
